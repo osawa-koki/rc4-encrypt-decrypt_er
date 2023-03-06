7c36551f1485c557aff52622bda65dd4b33772db
osawa-koki <github@osawa.dev>
2023-03-06 11:41:36 +0900

    JSDoc書いた。

--- src/RC4.ts
+++ src/RC4.ts
@@ -1,5 +1,10 @@
 
-function KSA(key: number[]): number[] {
+/**
+ * RC4暗号化における鍵スケジューリングアルゴリズム（KSA）を実行します。
+ * @param {number[]} key - 鍵のバイト配列。
+ * @returns {number[]} 初期化されたパーミュテーション値の配列。
+ */
+function KSA(key) {
   const S = new Array(256);
   for (let i = 0; i < 256; i++) {
     S[i] = i;
@@ -12,7 +17,12 @@ function KSA(key: number[]): number[] {
   return S;
 }
 
-function PRGA(S: number[]): number[] {
+/**
+ * RC4暗号化における疑似ランダムビットジェネレータ（PRGA）を実行します。
+ * @param {number[]} S - パーミュテーション値の配列。
+ * @returns {number[]} 生成された疑似ランダムビットの配列。
+ */
+function PRGA(S) {
   const K = [];
   let i = 0;
   let j = 0;
@@ -25,7 +35,13 @@ function PRGA(S: number[]): number[] {
   return K;
 }
 
-export function encrypt(data: string, key: string): string {
+/**
+ * 指定されたデータをRC4アルゴリズムを使用して暗号化します。
+ * @param {string} data - 暗号化するデータ。
+ * @param {string} key - 暗号化に使用するキー。
+ * @returns {string} 暗号化されたデータのBASE64エンコードされた文字列。
+ */
+export function encrypt(data, key) {
   const _data = new Uint8Array(new TextEncoder().encode(data));
   const _key = new Uint8Array(new TextEncoder().encode(key));
   const S = KSA(Array.from(_key));
@@ -37,6 +53,12 @@ export function encrypt(data: string, key: string): string {
   return btoa(String.fromCharCode.apply(null, result));
 }
 
+/**
+ * 指定されたBASE64エンコードされた文字列をRC4アルゴリズムを使用して復号化します。
+ * @param {string} data - 復号化するBASE64エンコードされた文字列。
+ * @param {string} key - 復号化に使用するキー。
+ * @returns {string} 復号化されたデータ。
+ */
 export function decrypt(data: string, key: string): string {
   const _data = new Uint8Array(
     atob(unescape(encodeURIComponent(data)))
